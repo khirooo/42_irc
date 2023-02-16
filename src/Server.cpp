f25124c40a6edfcbddde24db38540d4769f8b873
Kheireddine Fergani <kfergani@1-D-1.42heilbronn.de>
2023-02-16 07:46:30 +0100

    fixing getnameinfo

--- src/Server.cpp
+++ src/Server.cpp
@@ -150,7 +150,7 @@ void		Server::accept_connection(void)
 	if (new_sock != -1)
 	{
 		if (getnameinfo((struct sockaddr*)&client_addr, client_len, name, sizeof(name), 0, 0, NI_NAMEREQD) != 0)
-			std::cerr << "Error: getnameinfo " << strerror(errno) << std::endl;
+			std::cerr << "Error: getnameinfo " << errno << strerror(errno) << std::endl;
 	}
 	struct pollfd pfd;
 	pfd.fd = new_sock;
